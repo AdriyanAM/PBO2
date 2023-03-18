497ebae361b6b8b082a6e06f440f46329146a7bd
Adriyan Ari Muktiawan_210511177 <127741075+AdriyanAM@users.noreply.github.com>
2023-03-18 22:09:32 +0700

    Add files via upload

---
+++ PBO2/Latihan1/contoh1.py
@@ -0,0 +1,9 @@
+class Motor:
+    def __init__(self, merk, warna):
+        self.merk = merk
+        self.warna = warna
+    def info(self):
+        print(f"Motor {self.merk} berwarna {self.warna}")
+
+MotorA = Motor("Honda", "Biru")
+MotorA.info() # Output: Motor Yamaha berwarna Hitam
